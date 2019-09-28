14050d23a10919f572e8c3962f5477ff60a7f5ce
Giannis Nikiteas <giannis_nikiteas@hotmail.com>
2019-09-28 15:54:53 +0100

    Formatting changes

--- brunner_lattice_report_mc2019.py
+++ brunner_lattice_report_mc2019.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 import os
 import sys
 
@@ -25,10 +25,11 @@ from matplotlib2tikz import save as tikz_save
 plt.style.use('default')
 
 # Set the global font to be DejaVu Sans, size 10 (or any other sans-serif font of your choice!)
-matplotlib.rc('font',**{'family':'sans-serif','sans-serif':['DejaVu Sans'],'size':10})
+matplotlib.rc('font', **{'family': 'sans-serif',
+                         'sans-serif': ['DejaVu Sans'], 'size': 10})
 
 # Set the font used for MathJax - more on this later
-matplotlib.rc('mathtext',**{'default':'regular'})
+matplotlib.rc('mathtext', **{'default': 'regular'})
 
 
 class ExtractLB:
@@ -155,7 +156,9 @@ class ExtractLB:
                 sum(ratio[adapt::max_adapt])/len(ratio[adapt::max_adapt]))
             mode_ratio.append(stats.mode(ratio[adapt::max_adapt])[0])
 
-        return mean_ratio, mode_ratio
+        return mean_ratio, mode_ratio, ratio
+    
+    # def get_node_to_halo_3d(self, dir, )
 
     def get_strong_scaling(self, cores):
         """ Pass a list for the number of cores used in the study.
@@ -172,8 +175,11 @@ class ExtractLB:
         return t0 / (cn/c0 * tn) * 100
 
 
-#%%
+# %%
 CORES = [72, 240, 480, 1080]
+NODES = [3, 10, 20, 45]
+
+
 MAX_ADAPT = 5
 f_brunner_name = 'rad_radiant_noio'
 f_dogleg_name = 'rad_dogleg_noio'
@@ -192,30 +198,88 @@ brunner = ExtractLB()
 wall_t_b, lb_t_b, cdof_b = brunner.get_run_stats(
     dir_brunner, f_brunner_name, CORES)
 
-################################################################################
+
+# %% [markdown]
+# # Strong scaling results
+# %%
+# Strong scaling plot
+sc_dogleg = dogleg.get_strong_scaling(CORES)
+sc_brunner = brunner.get_strong_scaling(CORES)
+# TODO: change the number of processors to number of nodes and hence tick values
+fig, ax = plt.subplots()
+fig.canvas.set_window_title('Strong scaling dogleg problem')
+ax.plot(CORES, sc_dogleg, label='Dogleg', marker='o', color='blue')
+ax.plot(CORES, sc_brunner, label='Brunner', marker='s', color='red')
+ax.set(xlabel='Nodes', ylabel='Strong Scaling Performance %')
+ax.set_xticks(CORES)
+ax.set_xticklabels(NODES)
+ax.legend()
+fig.tight_layout()
+fig.savefig('./figures/strong_scaling_perf_wall_time_vs_nodes_adapt_5.png', dpi=300, pad_inches=0)
+fig.show()
+# %% [markdown]
+# # Time spent load balancing
+# %%
+fix, ax = plt.subplots(figsize=(8, 6))
+for cpu, c in zip([72, 240, 1080], ['red', 'blue', 'green']):
+    ax.plot(cdof[cpu], lb_t[cpu], marker='o',
+            label=f'{cpu} cores dogleg', color=c)
+    ax.plot(cdof_b[cpu], lb_t_b[cpu], marker='s',
+            linestyle='-.', label=f'{cpu} cores brunner', color=c)
+
+ax.set(xlabel='Continuous Degrees of Freedom', ylabel='Load Balance Time [s]')
+ax.legend()
+fig.tight_layout()
+# fig.savefig(fname)
+fig.show()
+# %% [markdown]
+# # Mode Node/Halo plot after adapting
+# %%
+_, ratio_mode_brunner, r_brunner = brunner.get_node_to_halo_ratio_stats(
+                                                             dir_brunner, 
+                                                             f_brunner_name, 
+                                                             MAX_ADAPT, 
+                                                             CORES[-1])
+_, ratio_mode_dogleg, r_dogleg = dogleg.get_node_to_halo_ratio_stats(dir_dogleg, 
+                                                           f_dogleg_name,
+                                                           MAX_ADAPT,
+                                                           CORES[-1])
+# # Mode of Node/Halo ratio distributions vs adapts                                                   
+fig, ax = plt.subplots(figsize=(8, 6))
+ax.plot(range(1, MAX_ADAPT+1), ratio_mode_dogleg,
+        label='Dogleg', color='blue', marker='o')
+ax.plot(range(1, MAX_ADAPT+1), ratio_mode_brunner,
+        label='Brunner', color='red', marker='s')
+ax.set(xlabel='Adapt step', ylabel='Node/Halo')
+ax.set_xticks(range(1, MAX_ADAPT+1))
+ax.legend()
+fig.show()
+
+# # Initial Node/Halo distribution
+fig, ax = plt.subplots(figsize=(8, 6))
+ax.hist(r_dogleg[0::MAX_ADAPT], bins=200, color='blue', label='Dogleg', alpha=0.7)
+ax.hist(r_brunner[0::MAX_ADAPT], bins=200, color='red', label='Brunner', alpha=0.7)
+ax.set(xlabel='Nodes/Halos', ylabel='Number of Partitions')
+ax.legend()
+fig.tight_layout()
+fig.show()
+# TODO: plot animmation for conference (maybe just use the 240 core case)
+# # After 5 adapt steps with load balancing distribution
+fig, ax = plt.subplots(figsize=(8, 6))
+ax.hist(r_dogleg[4::MAX_ADAPT], bins=200, color='blue', label='Dogleg', alpha=0.7)
+ax.hist(r_brunner[4::MAX_ADAPT], bins=200, color='red', label='Brunner', alpha=0.7)
+ax.set(xlabel='Nodes/Halos', ylabel='Number of Partitions')
+ax.legend()
+fig.tight_layout()
+fig.show()
 ################################################################################
 ################################ DOGLEG ########################################
 ################################################################################
-################################################################################
-#%% [markdown]
-# # DOGLEG PROBLEM
-#%%
-# Strong scaling plot
-fig2, ax2 = plt.subplots()
-fig2.canvas.set_window_title('Strong scaling dogleg problem')
-sc_dogleg = dogleg.get_strong_scaling(CORES)
-ax2.plot(CORES, sc_dogleg, label='Dogleg', marker='o', color='blue')
-
-ax2.set(xlabel='Number of Processors', ylabel='Strong Scaling Performance %')
-ax2.set_xticks(CORES)
-ax2.legend()
-fig2.tight_layout()
-# fig2.savefig(fname)
-fig2.show()
-#%% [markdown]
+# %% [markdown]
+# # DOGLEG
 # ## Get the mean and mode of node distribution for partition
 ################################################################################
-#%%
+# %%
 halo_mean, halo_mode, halos = dogleg.get_halo_stats(dir_dogleg,
                                                     f_dogleg_name,
                                                     MAX_ADAPT, CORES[-1])
@@ -244,7 +308,7 @@ ax1.plot(range(1, MAX_ADAPT+1), halo_mean,
 ax1.set(xlabel='Adapt step', ylabel='Size')
 ax1.legend()
 
-# Print node statistics 
+# Print node statistics
 ax2.plot(range(1, MAX_ADAPT+1), node_mode,
          label='Node mode', color='blue', marker='X')
 ax2.plot(range(1, MAX_ADAPT+1), node_mean,
@@ -254,7 +318,7 @@ ax2.legend()
 fig.show()
 
 # Calculate the mode of Node/Halo vs Mean
-ratio_mean, ratio_mode = dogleg.get_node_to_halo_ratio_stats(
+ratio_mean, ratio_mode, __ = dogleg.get_node_to_halo_ratio_stats(
     dir_dogleg, f_dogleg_name, MAX_ADAPT, CORES[-1])
 fig, ax = plt.subplots()
 ax.plot(range(1, MAX_ADAPT+1), ratio_mean,
@@ -266,77 +330,119 @@ ax.legend()
 fig.show()
 
 ################################################################################
-################################################################################
 ############################### BRUNNER ########################################
 ################################################################################
-################################################################################
-#%% [markdown]
+# %% [markdown]
 # # BRUNNER LATTICE PROBLEM
-#%%
-fig2, ax2 = plt.subplots()
-fig2.canvas.set_window_title('Strong scaling brunner problem')
-sc_brunner = brunner.get_strong_scaling(CORES)
-
-ax2.plot(CORES, sc_brunner, label='strong scaling', marker='+')
-ax2.set(xlabel='Number of Processors', ylabel='Strong Scaling Performance %')
-ax2.set_xticks(CORES)
-# ax2.legend()
-fig2.tight_layout()
-# fig2.savefig(fname)
-fig2.show()  # %%
-
-#%% [markdown]
+# %% [markdown]
 # ## Get the mean and mode of node distribution for partition
+# %%
 ################################################################################
-#%%
-halo_mean, halo_mode, halos = brunner.get_halo_stats(dir_brunner,
+halo_mean_b, halo_mode_b, halos_b = brunner.get_halo_stats(dir_brunner,
                                                      f_brunner_name,
                                                      MAX_ADAPT, CORES[-1])
-node_mean, node_mode, nodes = brunner.get_partition_stats(dir_brunner,
+node_mean_b, node_mode_b, nodes_b = brunner.get_partition_stats(dir_brunner,
                                                           f_brunner_name,
                                                           MAX_ADAPT, CORES[-1])
 
 # Plot histogram distribution o nodes/halos ratio
 fig1, ax1 = plt.subplots()
-ax1.hist(nodes[0::MAX_ADAPT]/halos[0::MAX_ADAPT], 200,
+ax1.hist(nodes_b[0::MAX_ADAPT]/halos_b[0::MAX_ADAPT], 200,
          label='core 1080', color='red', alpha=0.7)
-ax1.hist(nodes[4::MAX_ADAPT]/halos[4::MAX_ADAPT], 200,
+ax1.hist(nodes_b[4::MAX_ADAPT]/halos_b[4::MAX_ADAPT], 200,
          label='core 1080, adapt 5', color='blue', alpha=0.7)
 
 ax1.set(xlabel='Nodes/Halos', ylabel='Number of Partitions')
 ax1.legend()
 fig1.tight_layout()
-# fig1.savefig(fname)
 fig1.show()
 
 # Print statistics for halos
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
-ax1.plot(range(1, MAX_ADAPT+1), halo_mode,
+ax1.plot(range(1, MAX_ADAPT+1), halo_mode_b,
          label='halo mode', color='blue', marker='X')
-ax1.plot(range(1, MAX_ADAPT+1), halo_mean,
+ax1.plot(range(1, MAX_ADAPT+1), halo_mean_b,
          label='halo mean', color='red', marker='+')
 ax1.set(xlabel='Adapt step', ylabel='Size')
 ax1.legend()
 
 # Print statistics for node partitions
-ax2.plot(range(1, MAX_ADAPT+1), node_mode,
+ax2.plot(range(1, MAX_ADAPT+1), node_mode_b,
          label='Node mode', color='blue', marker='X')
-ax2.plot(range(1, MAX_ADAPT+1), node_mean,
+ax2.plot(range(1, MAX_ADAPT+1), node_mean_b,
          label='Node mean', color='red', marker='+')
 ax2.set(xlabel='Adapt step', ylabel='Size')
 ax2.legend()
 
 fig.show()
 
-# Calculate the mode of Node/Halo vs Mean
-ratio_mean, ratio_mode = brunner.get_node_to_halo_ratio_stats(
+# Calculate the mode of Node/Halo,  Mean vs adapt
+ratio_mean_b, ratio_mode_b, __ = brunner.get_node_to_halo_ratio_stats(
     dir_brunner, f_brunner_name, MAX_ADAPT, CORES[-1])
 fig, ax = plt.subplots()
-ax.plot(range(1, MAX_ADAPT+1), ratio_mean,
+ax.plot(range(1, MAX_ADAPT+1), ratio_mean_b,
         label='ratio mean', color='blue', marker='X')
-ax.plot(range(1, MAX_ADAPT+1), ratio_mode,
+ax.plot(range(1, MAX_ADAPT+1), ratio_mode_b,
         label='ratio mode', color='red', marker='+')
 ax.set(xlabel='Adapt step', ylabel='Node/Halo')
 ax.legend()
 fig.show()
+
+#%% [markdown]
+# # Strong scaling and absolute performance of the Load Balance time
+#%%
+# Get the load balance time for the last adapt step
+lb_last_adapt = [lb_t[cpu][-1] for cpu in CORES]
+lb_last_adapt_b = [lb_t_b[cpu][-1] for cpu in CORES]    # Brunner problem
+# Strong scaled load balance times
+ss_lb_last_adapt = [dogleg.strong_scaling(lb_last_adapt[0], min(CORES), time, cpu) for time, cpu in zip(lb_last_adapt, CORES)]
+ss_lb_last_adapt_b = [brunner.strong_scaling(lb_last_adapt_b[0], min(CORES), time, cpu) for time, cpu in zip(lb_last_adapt_b, CORES)]
+# This plots the strong scaling of the load balance time
+fig, ax = plt.subplots()
+ax.plot(CORES, ss_lb_last_adapt, color='blue', marker='o', label='Dogleg')
+ax.plot(CORES, ss_lb_last_adapt_b, color='red', marker='s', label='Brunner')
+ax.set(xlabel='Nodes', ylabel=r'Strong scaling % of Load Balance Time')
+ax.set_xticks(CORES)
+ax.set_xticklabels(NODES)
+ax.legend()
+fig.tight_layout()
+fig.show()
+fig.savefig('./figures/strong_scaling_perf_lb_time_adapt_5.png', dpi=300, pad_inches=0)
+
+# Plots the absolute load balance time
+fig, ax = plt.subplots() #figsize=(8,6)
+ax.plot(CORES, lb_last_adapt, color='blue', marker='o',label='Dogleg')
+ax.plot(CORES, lb_last_adapt_b, color='red', marker='s', label='Brunner')
+ax.set(xlabel='Nodes', ylabel='Load Balance Time [s]')
+ax.set_xticks(CORES)
+ax.set_xticklabels(NODES)
+ax.legend()
+fig.tight_layout()
+fig.show()
+fig.savefig('./figures/lb_time_vs_cpu_adapt_5.png', dpi=300, pad_inches=0)
+
+#%% [markdown]
+# # Generate frames for the 10 adapt step case (which was run on 10 Nodes)
+#%%
+_, ratio_mode_brunner, r_brunner = brunner.get_node_to_halo_ratio_stats(
+                                                             dir_brunner, 
+                                                             f_brunner_name, 
+                                                             MAX_ADAPT, 
+                                                             CORES[-1])
+_, ratio_mode_dogleg, r_dogleg = dogleg.get_node_to_halo_ratio_stats(dir_dogleg, 
+                                                           f_dogleg_name,
+                                                           MAX_ADAPT,
+                                                          CORES[-1])
+
+# Generate all frames for adapt
+for i in range(MAX_ADAPT):
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.hist(r_dogleg[i::MAX_ADAPT], bins=200, color='blue', label='Dogleg', alpha=0.7)
+    ax.hist(r_brunner[i::MAX_ADAPT], bins=200, color='red', label='Brunner', alpha=0.7)
+    ax.set(xlabel='Nodes/Halos', ylabel='Number of Partitions')
+    ax.legend()
+    fig.tight_layout()
+    fig.show()
+    fig.savefig(f'node_halo_dist-{i}.png', dpi=300, pad_inches=0)
+
 #%%
